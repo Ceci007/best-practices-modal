c027510b77c8cbe7aca319a449ba04adea526d09
Ceci Benitez <cecibenitez@Cecis-MacBook-Pro.local>
2022-06-18 19:14:10 -0400

    Added pokemon description

--- src/App.js
+++ src/App.js
@@ -10,10 +10,15 @@ function App() {
       <button className="btn-primary" onClick={() => modalRef.current.open()}>
         Open Modal
       </button>
-      <Modal ref={modalRef}>
+      <Modal className="flex-container" ref={modalRef}>
         <div className="image-container">
           <img src="/dragonite.png" alt="pokemon draw" />
         </div>
+        <h3 className="title">Dragonite, the dragon type</h3>
+        <p className="text">
+          Dragonite is a Dragon/Flying type Pokémon introduced in Generation 1.
+          It is known as the Dragon Pokémon.
+        </p>
       </Modal>
     </div>
   );
